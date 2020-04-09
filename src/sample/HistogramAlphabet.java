72e0634225926e66f2aef6636edf8992411abbcb
zealptl <zealpatel06@gmail.com>
2020-04-09 18:26:04 -0400

    added HistogramAlphabet class and its methods

---
+++ src/sample/HistogramAlphabet.java
@@ -0,0 +1,83 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
+
+public class HistogramAlphabet {
+
+    //Variables
+    private LinkedHashMap<Character, Integer> frequency;
+    private LinkedHashMap<Character, Double> probability;
+
+    //Getters
+    public LinkedHashMap<Character, Integer> getFrequency() { return frequency; }
+    public LinkedHashMap<Character, Double> getProbability() { return probability; }
+
+    //Setters
+    public void setFrequency(String filelocation) throws IOException {
+        this.frequency = this.readFile(filelocation);
+    }
+    public void setProbability() {
+        this.probability = this.findProbabilities();
+    }
+
+    //Methods
+    public LinkedHashMap<Character, Integer> readFile(String fileLocation) throws IOException {
+        LinkedHashMap<Character, Integer> temp = new LinkedHashMap<>();
+        for (char c = 'a'; c <= 'z'; c++) {
+            temp.put(c,0);
+        }
+        File f = new File(fileLocation);
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        int c = 0;
+        while ((c = br.read()) != -1) {
+            char readChar = (char) c;
+            if (Character.isLetter(readChar)) {
+                readChar = Character.toLowerCase(readChar);
+                temp.put(readChar, temp .get(readChar) + 1);
+            }
+        }
+
+        LinkedHashMap<Character, Integer> sorted = temp
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(
+                        toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2) -> e2, LinkedHashMap::new)
+                );
+
+        return sorted;
+    }
+
+//    public LinkedHashMap<Character, Integer> sortFrequency() {
+//        LinkedHashMap<Character, Integer> sorted = this.frequency
+//                .entrySet()
+//                .stream()
+//                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+//                .collect(
+//                        toMap(Map.Entry::getKey, Map.Entry::getValue, (e1,e2) -> e2, LinkedHashMap::new)
+//                );
+//        return sorted;
+//    }
+
+    public LinkedHashMap<Character, Double> findProbabilities() {
+        LinkedHashMap<Character, Double> temp = new LinkedHashMap<>();
+        int total = 0;
+        for (int value: this.frequency.values())
+            total += value;
+        System.out.println(total);
+
+        for (Map.Entry<Character, Integer> entry: this.frequency.entrySet()) {
+            double entryProbability = (double) entry.getValue() / total;
+            temp.put(entry.getKey(), entryProbability);
+        }
+
+        return temp;
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,58 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 
 public class Main extends Application {
 
+//    public LinkedHashMap<Character, Integer> readFile(String fileLocation) throws IOException {
+//        LinkedHashMap<Character, Integer> temp = new LinkedHashMap<>();
+//        for (char c = 'a'; c <= 'z'; c++) {
+//            temp.put(c,0);
+//        }
+//        File f = new File(fileLocation);
+//        FileReader fr = new FileReader(f);
+//        BufferedReader br = new BufferedReader(fr);
+//        int c = 0;
+//        while ((c = br.read()) != -1) {
+//            char readChar = (char) c;
+//            if (Character.isLetter(readChar)) {
+//                readChar = Character.toLowerCase(readChar);
+//                temp.put(readChar, temp .get(readChar) + 1);
+//            }
+//        }
+//        return temp;
+//    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("My Pie Chart");
+        Group root = new Group();
+        int w = 600, h = 400;
+        Canvas canvas = new Canvas(w, h);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        HistogramAlphabet data = new HistogramAlphabet();
+        data.setFrequency("./assets/aliceInWonderland.txt");
+        System.out.println(data.getFrequency());
+
+        data.setProbability();
+        System.out.println(data.getProbability());
+
+
+        root.getChildren().add(canvas);
+        Scene sc = new Scene(root, w, h);
+        primaryStage.setScene(sc);
         primaryStage.show();
     }
 
